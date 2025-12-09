6ce5f77e80f95f119ccd42923f5f7c4a54f40fb1
khalilurRahmanS <khalilurrahmansaeed10@gmail.com>
2025-12-09 23:04:23 +0530

    updated Jenkinsfile

--- app.py
+++ app.py
@@ -1,11 +1,7 @@
 from flask import Flask
-from waitress import serve
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
     return "Hello World from Jenkins CI/CD Pipeline!"
-
-if __name__ == "__main__":
-    serve(app, host="0.0.0.0", port=5000)
