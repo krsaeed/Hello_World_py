1a4f4d59b482e87fe7223617bcd532b43f942e3e
khalilurRahmanS <khalilurrahmansaeed10@gmail.com>
2025-12-24 13:58:27 +0530

    Jenkinsfile updated

--- app.py
+++ app.py
@@ -3,5 +3,10 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
-def home():
-    return "Hello World from Jenkins CI/CD Pipeline!"
+
+def hello():
+    return "Hello from Jenkins + Docker + WSL!"
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
+
